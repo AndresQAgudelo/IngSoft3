16ccd0ac4e7baa7030ad8290c5dccccf31e4e171
unknown <andres.quebrada@utp.edu.co>
2023-05-10 13:56:49 -0500

    Primera vista Portal de empleo

---
+++ Bolsa_trabajo/System/footer.php
@@ -0,0 +1,94 @@
+			
+			<footer class="footer-wrapper">
+			
+            <div class="main-footer">
+            
+                <div class="container">
+                
+                    <div class="row">
+                    
+                        <div class="col-sm-12 col-md-9">
+                        
+                            <div class="row">
+                            
+                                <div class="col-sm-6 col-md-4">
+                                
+                                    <div class="footer-about-us">
+                                        <h5 class="footer-title">Acerca</h5>
+                                        <p>Portal de empleo</p>
+                                    
+                                    </div>
+
+                                </div>
+                                
+                                <div class="col-sm-6 col-md-5 mt-30-xs">
+                                    <h5 class="footer-title">Enlaces Rapidos</h5>
+                                    <ul class="footer-menu clearfix">
+                                        <li><a href="./">Inicio</a></li>
+                                        <li><a href="job-list.php">Lista de empleos</a></li>
+                                        <li><a href="employers.php">Empresas</a></li>
+                                        <li><a href="employees.php">Personas</a></li>
+                                        <li><a href="contact.php">Contacto</a></li>
+                                        <li><a href="#">Ir Arriba</a></li>
+
+                                    </ul>
+                                
+                                </div>
+
+                            </div>
+
+                        </div>
+                        
+                        <div class="col-sm-12 col-md-3 mt-30-sm">
+                        
+                            <h5 class="footer-title">Contacto</h5>
+                            
+                            <p>Universidad Tecnologica de Pereira</p>
+                            <p>Correo Electrónico : <a href="utpp@gmail.com">utp@gmail.com</a></p>
+                            <p>Celular : <a href="">3014001500</a></p>
+                            
+
+                        </div>
+
+                        
+                    </div>
+                    
+                </div>
+                
+            </div>
+
+            <div class="bottom-footer">
+				
+                <div class="container">
+                
+                    <div class="row">
+                    
+                        <div class="col-sm-4 col-md-4">
+                
+                            <p class="copy-right">&#169; Copyright <?php echo date('Y'); ?></p>
+                            
+                        </div>
+                        
+                        <div class="col-sm-4 col-md-4">
+                        
+                            <ul class="bottom-footer-menu">
+                                <li><a >Desarrollado por @Utp-IngSofIII</a></li>
+                            </ul>
+                        
+                        </div>
+                        
+                        <div class="col-sm-4 col-md-4">
+                            <ul class="bottom-footer-menu for-social">
+                                <li><a href="<?php echo "$tw"; ?>"><i class="ri ri-twitter" data-toggle="tooltip" data-placement="top" title="twitter"></i></a></li>
+                                <li><a href="<?php echo "$fb"; ?>"><i class="ri ri-facebook" data-toggle="tooltip" data-placement="top" title="facebook"></i></a></li>
+                                <li><a href="<?php echo "$ig"; ?>"><i class="ri ri-instagram" data-toggle="tooltip" data-placement="top" title="instagram"></i></a></li>
+                            </ul>
+                        </div>
+                    
+                    </div>
+
+                </div>
+                
+            </div>
+        
+        </footer>
